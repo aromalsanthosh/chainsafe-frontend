b898bdf61ba21d1815248da13a12be6d65c8e7fe
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-04-10 13:00:42 +0530

    Update police, admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,8 @@
 import Navbar from "../components/Navbar";
 import Head from "next/head";
 import Footer from "../components/Footer";
-
+import Welcome from "../components/Welcome";
+import Wallet from "../components/Wallet";
 export default function Admin() {
   return (
     <div>
@@ -10,6 +11,9 @@ export default function Admin() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Navbar />
+      <Welcome name="Admin" />
+      <Wallet address="0x1234567890abcdef" balance="5.4321" />
+      
       <div className="hero min-h-screen bg-base-200">
         Admin Page
       </div>
--- pages/police.js
+++ pages/police.js
@@ -1,7 +1,8 @@
 import Navbar from "../components/Navbar";
 import Head from "next/head";
 import Footer from "../components/Footer";
-
+import Welcome from "../components/Welcome";
+import Wallet from "../components/Wallet";
 export default function Shop() {
   return (
     <div>
@@ -10,6 +11,8 @@ export default function Shop() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Navbar />
+      <Welcome name="Police" />
+      <Wallet address="0x1234567890abcdef" balance="5.4321" />
       <div className="hero min-h-screen bg-base-200">
         Police Page
       </div>
