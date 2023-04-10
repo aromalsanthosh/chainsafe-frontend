082c928a15652b928af17916bea332b9e80dbcaa
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-04-10 10:39:38 +0530

    fix apos

--- components/Welcome.js
+++ components/Welcome.js
@@ -4,7 +4,7 @@ function Welcome({ name }) {
   return (
     <div className="bg-blue-500 text-white p-4">
       <h1 className="text-xl font-bold">Welcome, {name}!</h1>
-      <p className="text-sm">We're glad to have you here.</p>
+      <p className="text-sm">We&apos;re glad to have you here.</p>
     </div>
   );
 }
