19d4c80337e9dfb028cb11a074fd6acfb708a148
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-04-16 22:34:24 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,31 @@ import Footer from "../components/Footer";
 import Link from "next/link";
 import Web3 from "web3";
 import { useState, useEffect } from "react";
+import Insurance from "../abis/Insurance.json";
 
 export default function Home() {
-  const [isWalletConnected, setIsWalletConnected] = useState(false);
   const [account, setAccount] = useState("");
+  const [productCount, setProductCount] = useState(0);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [accountBalance, setAccountBalance] = useState(0);
   const [sellerAccount, setSellerAccount] = useState("");
   const [policeAccount, setPoliceAccount] = useState("");
   const [repairAccount, setRepairAccount] = useState("");
+  const [insurance, setInsurance] = useState(null);
 
-  const handleConnectWallet = async () => {
-    await loadWeb3();
-    await getAllAccounts();
-    await loadBlockchainData();
-  };
-
+  const handleClick = async (e) => {
+    const load = async () => {
+      await loadWeb3();
+      await getAllAccounts();
+      await loadBlockchainData();
+    };
+    load();
+  }
 
   useEffect(() => {
     window.ethereum.on("accountsChanged", function (accounts) {
       setAccount(accounts[0]);
-      console.log(accounts[0]);
       window.location.reload();
     });
   }, []);
@@ -32,12 +36,7 @@ export default function Home() {
   const loadWeb3 = async () => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
-      try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-      } catch (error) {
-        // User denied account access...
-        console.error(error);
-      }
+      await window.ethereum.request({ method: "eth_requestAccounts" });
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
     } else {
@@ -46,7 +45,6 @@ export default function Home() {
       );
     }
   };
-  
 
   const getAllAccounts = async () => {
     let web3 = new Web3(window.ethereum);
@@ -66,6 +64,142 @@ export default function Home() {
     );
     accountBalance = web3.utils.fromWei(accountBalance, "ether");
     setAccountBalance(accountBalance);
+
+    const insuranceContract = new web3.eth.Contract(
+      Insurance.abi,
+      "0x752cA0Ef9756835f2e9d422dbB0360dEbD8D0317"
+    );
+
+    setInsurance(insuranceContract);
+
+    if (insuranceContract.options.address) {
+      console.log(
+        "Connected to smart contract at",
+        insuranceContract.methods.productCount().call().then(console.log)
+      );
+    } else {
+      console.log("Failed to connect to smart contract");
+    }
+
+    const methods = insuranceContract.methods;
+    const methodNames = Object.getOwnPropertyNames(
+      Object.getPrototypeOf(methods)
+    );
+    console.log(methodNames);
+
+    if (insuranceContract) {
+      const productCount = await insuranceContract.methods
+        .productCount()
+        .call();
+      setProductCount(productCount);
+
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await insuranceContract.methods.products(i).call();
+        setProducts((prevProducts) => [...prevProducts, product]);
+      }
+      setLoading(false);
+    } else {
+      window.alert("Insurance contract not deployed to detected network.");
+    }
+  };
+
+  const createProduct = (name, price, insurancePrice) => {
+    setLoading(true);
+    insurance.methods
+      .createProduct(name, price, insurancePrice)
+      .send({ from: account })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
+  };
+
+  const purchaseProduct = (id, price) => {
+    setLoading(true);
+    insurance.methods
+      .purchaseProduct(id, repairAccount, policeAccount)
+      .send({ from: account, value: price })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+      });
+  };
+
+  const purchaseInsurance = (id, insurancePrice) => {
+    setLoading(true);
+    insurance.methods
+      .purchaseInsurance(id, repairAccount, policeAccount)
+      .send({ from: account, value: insurancePrice })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
+  };
+
+  const policeClaim = (id) => {
+    setLoading(true);
+    insurance.methods
+      .claimPolice(id, repairAccount, policeAccount)
+      .send({ from: account })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
+  };
+
+  const repairClaim = (id) => {
+    setLoading(true);
+    insurance.methods
+      .claimRepair(id, repairAccount, policeAccount)
+      .send({ from: account })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
+  };
+
+  const stolen = (id) => {
+    setLoading(true);
+    insurance.methods
+      .stolen(id, repairAccount, policeAccount)
+      .send({ from: account })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
+  };
+
+  const repaired = (id) => {
+    setLoading(true);
+    insurance.methods
+      .repaired(id, repairAccount, policeAccount)
+      .send({ from: account })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
+  };
+
+  const reimburse = (id, productPrice) => {
+    setLoading(true);
+    insurance.methods
+      .reimburse(id, repairAccount, policeAccount)
+      .send({ from: account, value: productPrice })
+      .on("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
+  };
+
+  const payRepairShop = (id, repairFee) => {
+    setLoading(true);
+    insurance.methods
+      .payRepairShop(id, repairAccount, policeAccount)
+      .send({ from: account, value: repairFee })
+      .once("receipt", (receipt) => {
+        console.log(receipt);
+        setLoading(false);
+      });
   };
 
   return (
@@ -97,7 +231,6 @@ export default function Home() {
               </p>
               <Link href="/">
                 <button
-                  onClick={handleConnectWallet}
                   id="#connectwallet"
                   className="md:mr-6 mb-6 md:w-44 w-full btn btn-primary px-8 bg-primary normal-case font-normal rounded-none"
                 >
