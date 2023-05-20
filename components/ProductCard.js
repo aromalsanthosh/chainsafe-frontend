20610a1b29d26bb080e075038eb8a153872a319f
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-05-20 11:40:35 +0530

    Add purchase functionality

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,22 +1,43 @@
 import React from "react";
 import Image from "next/image";
+import { useContext } from "react";
+import { TransactionContext } from "../context/TransactionContext";
 
-function ProductCard({ imageUrl, brand, model, priceInEth }) {
+function ProductCard({ product }) {
+  const { addProduct, account  } = useContext(TransactionContext);
+  
+  const handleBuyNow = () => {
+    addProduct(
+      product.productId.toString(),
+      product.brand,
+      product.model,
+      product.image,
+      product.price,
+      new Date().toLocaleDateString("en-GB").replace(/\//g, "-"),
+      account
+    );
+    console.log("ProductID: ", typeof product.productId.toString());
+    
+  };
+      
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-md">
       <div className="relative h-48 w-full">
         <Image
-          src={imageUrl}
+          src={product.image}
           alt="Product"
           layout="fill"
           objectFit="cover"
         />
       </div>
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{brand}</div>
-        <p className="text-gray-700 text-base mb-2">{model}</p>
-        <p className="text-gray-700 text-base font-bold mb-2">{priceInEth} ETH</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <div className="font-bold text-xl mb-2">{product.brand}</div>
+        <p className="text-gray-700 text-base mb-2">{product.model}</p>
+        <p className="text-gray-700 text-base font-bold mb-2">{product.price} ETH</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => handleBuyNow()}
+        >
           Buy Now
         </button>
       </div>
