182c93206ff24249b73bf44cb3e90e2096ecbdf9
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-06-17 07:21:24 +0530

    update s3 region

--- pages/shop.js
+++ pages/shop.js
@@ -25,6 +25,7 @@ import AWS from 'aws-sdk';
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'ap-south-1',
 });
 
 
